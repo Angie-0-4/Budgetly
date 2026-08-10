1bb88efadc656e3eb2b71bc54b830057a465305c
Anjelika Vasic <anjelikavasic@AirvonAnjelika.fritz.box>
2026-08-10 15:43:14 +0200

    Backend: Datenmodell angelegt

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,9 @@ const connectDB = async () => {
         const conn = await mongoose.conect('mongodb://127.0.0.1:27017/budgetly');
         console.log(`MongoDB verbunden: ${conn.connection.host}`);
     } catch (error) {
-        console.error
+        console.error(`Fehler bei der DB-Verbindung: ${error.message}`);
+        process.exit(1);
     }
-}
+};
+
+module.exports = connectDB;
