e9261f6a8e2b9ff723dd8cc768ee29496de53bfd
Anjelika Vasic <anjelikavasic@AirvonAnjelika.fritz.box>
2026-08-11 23:53:08 +0200

    Backend: seed Test

--- backend/seed.js
+++ backend/seed.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const Transaction = require(':/models/Transaction');
+const Transaction = require('./models/Transaction');
 
 const sampleTransaction = [
     { title: 'Gehalt', amount: 2500, type: 'income', category: 'Gehalt' },
     { title: 'Supermarkt Einkaud', amount: 85.50, type: 'expense', category: 'Lebensmittel' },
-    { title: 'Fitnessstudio', amount: 30, tyoe: 'expense', category: 'Freizeit' },
+    { title: 'Fitnessstudio', amount: 30, type: 'expense', category: 'Freizeit' },
     { title: 'Stromrechnung', amount: 65, type: 'expense', category: 'Haushalt' }
 ];
 
@@ -17,7 +17,7 @@ const seedDB = async () => {
         await Transaction.deleteMany({});
 
         // Neue Testdaten einfügen
-        await Transaction.instertMany(sampleTransaction);
+        await Transaction.insertMany(sampleTransaction);
         console.log('Testdaten erfolgreich geladen!');
 
         await mongoose.disconnect();
