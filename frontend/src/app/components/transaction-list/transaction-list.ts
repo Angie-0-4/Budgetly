e0a918ed00c06737fe25ce91ab38a3d1b42cd03f
Anjelika Vasic <anjelikavasic@MacBook-Air-von-Anjelika.local>
2026-08-20 03:16:11 +0200

    frontend: holt daten über den service

--- frontend/src/app/components/transaction-list/transaction-list.ts
+++ frontend/src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import  { TransactionService, Transaction } from '../../services/transaction.service';
+import { errorContext } from 'rxjs/internal/util/errorContext';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './transaction-list.html',
   styleUrl: './transaction-list.css',
 })
-export class TransactionList {
+export class TransactionList implements OnInit{
+transactions: Transaction[] = [];
 
+constructor(private transactionService: TransactionService) {}
+
+//automatischer aufruf
+ngOnInit(): void {
+    this.loadTransactions();
+}
+
+//Dtaen laden
+loadTransactions():void {
+  this.transactionService.getTransactions().subscribe({
+    next: (data) => this.transactions = data,
+    error: (err) => console.error('Fehler beim Laden: ', err)
+  });
+}
+
+//Löschen Button
+deleteTransaction(id: string | undefined): void {
+  if (!id) return;
+  this.transactionService.deleteTransaction(id).subscribe({
+    next: () => {
+      //nach dem löschen liste neu laden
+      this.loadTransactions();
+    },
+    error: (err) => console.error('Fehler beim Löschen: ', err)
+  });
+}
 }
