b41fef9f75a10634c2ed32126ba624badb80bf2b
Anjelika Vasic <anjelikavasic@MacBook-Air-von-Anjelika.local>
2026-08-20 05:02:42 +0200

    frontend: TransactionForm hinzugefügt

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransactionList } from './components/transaction-list/transaction-list';
+import { TransactionForm } from './components/transaction-form/transaction-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TransactionList],
+  imports: [RouterOutlet, TransactionList, TransactionForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'Budgetly';
 }
