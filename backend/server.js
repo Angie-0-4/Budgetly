3adcae6394ada34444527860370f35683486ba2e
Anjelika Vasic <anjelikavasic@MacBook-Air-von-Anjelika.local>
2026-08-29 05:15:05 +0200

    backend: serer auth hinzugefügt

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
