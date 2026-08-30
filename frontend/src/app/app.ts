5cc4940fe41e59021e5541cf8f7a0c956b8f655d
Anjelika Vasic <anjelikavasic@MacBook-Air-von-Anjelika.local>
2026-08-30 03:19:58 +0200

    fronted: auth into apps connection und so

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransactionList } from './components/transaction-list/transaction-list';
 import { TransactionForm } from './components/transaction-form/transaction-form';
+import { AuthComponent } from './components/auth/auth';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TransactionList, TransactionForm],
+  standalone: true,
+  imports: [RouterOutlet, TransactionList, TransactionForm, AuthComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   title = 'Budgetly';
+  currentUser: string | null = null;
 
+  constructor(public authService: AuthService) {
+
+    ngOnInit(): void {
+      this.authService.currentUser$.subscribe((user) => {
+        this.currentUser = user;
+      });
+    }
+    
+    logout(): void {
+      this.authService.logout()
+    }
 }
+} 
